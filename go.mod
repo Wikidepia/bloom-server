3c67e99f085b9370abeeb9742a3e185eda430bf4
Akmal <magchuz@pm.me>
2021-07-16 08:35:18 +0700

    Use xxh3

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module main
 go 1.16
 
 require (
-	github.com/cespare/xxhash v1.1.0 // indirect
-	github.com/cespare/xxhash/v2 v2.1.1 // indirect
-	github.com/greatroar/blobloom v0.7.0 // indirect
-	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/greatroar/blobloom v0.7.0
+	github.com/json-iterator/go v1.1.11
+	github.com/zeebo/xxh3 v0.12.0
 )
--- main.go
+++ main.go
@@ -6,17 +6,16 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
 
-	xxhash "github.com/cespare/xxhash/v2"
 	jsoniter "github.com/json-iterator/go"
+	"github.com/zeebo/xxh3"
 
 	"github.com/greatroar/blobloom"
 )
 
 var json = jsoniter.ConfigFastest
 
-func filter(f *blobloom.SyncFilter, file io.Reader) string {
+func filter(f *blobloom.Filter, file io.Reader) string {
 	var data []interface{}
 	var ret []interface{}
 	value, err := ioutil.ReadAll(file)
@@ -27,8 +26,7 @@ func filter(f *blobloom.SyncFilter, file io.Reader) string {
 	for _, value := range data {
 		data_in := value.([]interface{})
 		text := data_in[0].(string)
-		bytesText := []byte(text)
-		hashText := xxhash.Sum64(bytesText)
+		hashText := xxh3.Hash([]byte(text))
 		if !f.Has(hashText) {
 			f.Add(hashText)
 			ret = append(ret, data_in)
@@ -45,27 +43,18 @@ func filter(f *blobloom.SyncFilter, file io.Reader) string {
 }
 
 func main() {
-	f := blobloom.NewSyncOptimized(blobloom.Config{
-		Capacity: 10_000_000_000, // Expected number of keys.
-		FPRate:   0.01,           // One in 10000 false positives is acceptable.
+	f := blobloom.NewOptimized(blobloom.Config{
+		Capacity: 10_000_000_000,
+		FPRate:   0.05,
 	})
-	for i := 1; i < 10_000_000_000; i++ {
-		go func() {
-			f.Add(xxhash.Sum64([]byte(strconv.Itoa(i))))
-			if i%100_000 == 0 {
-				println(i)
-			}
-		}()
-	}
 
-	http.HandleFunc("/filter_url/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/bloom/", func(w http.ResponseWriter, r *http.Request) {
 		file, _, err := r.FormFile("file")
 		if err != nil {
 			log.Fatal(err)
 		}
-		result := filter(f, file)
-		fmt.Fprint(w, result)
+		fmt.Fprint(w, filter(f, file))
 	})
 	println("Server started")
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
