8b22d26767fbc9669a45d8f22a5cbda074320625
Akmal <magchuz@pm.me>
2021-08-11 15:49:40 +0700

    Use redisbloom-go

--- go.mod
+++ go.mod
@@ -3,11 +3,8 @@ module main
 go 1.16
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/greatroar/blobloom v0.7.0
-	github.com/klauspost/compress v1.13.1
+	github.com/RedisBloom/redisbloom-go v1.0.0
+	github.com/gomodule/redigo v1.8.5
 	github.com/rs/zerolog v1.23.0
-	github.com/zeebo/xxh3 v0.12.0
-	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200605160147-a5ece683394c // indirect
 )
--- main.go
+++ main.go
@@ -7,28 +7,51 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 	"strings"
+	"unsafe"
 
-	"github.com/greatroar/blobloom"
+	redisbloom "github.com/RedisBloom/redisbloom-go"
+	"github.com/gomodule/redigo/redis"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
-	"github.com/zeebo/xxh3"
 )
 
-var bloomFilter = blobloom.NewSyncOptimized(blobloom.Config{
-	Capacity: 10_000_000_000,
-	FPRate:   0.05,
-})
-var newLines = []byte("\n")
+var (
+	pool = &redis.Pool{Dial: func() (redis.Conn, error) {
+		return redis.Dial("tcp", "localhost:6379", redis.DialPassword(""))
+	}}
+	client   = redisbloom.NewClientFromPool(pool, "bloom-cah")
+	newLines = []byte("\n")
+)
+
+// https://github.com/golang/go/issues/25484#issuecomment-391415660
+func ByteSlice2String(bs []byte) string {
+	return *(*string)(unsafe.Pointer(&bs))
+}
 
 func filter(file io.Reader) string {
 	var sb strings.Builder
+	var lines []string
+	var existURLs []string
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		hashText := xxh3.Hash(scanner.Bytes())
-		if !bloomFilter.Has(hashText) {
-			bloomFilter.Add(hashText)
-			sb.Write(append(scanner.Bytes(), newLines...))
+		lines = append(lines, ByteSlice2String(scanner.Bytes()))
+	}
+	result, err := client.BfExistsMulti("urls", lines)
+	if err != nil {
+		log.Info().Err(err)
+	}
+
+	for i := 0; i < len(result); i++ {
+		if result[i] == 0 {
+			sb.WriteString(lines[i])
+			sb.Write(newLines)
+			existURLs = append(existURLs, lines[i])
 		}
+	}
+	_, err = client.BfAddMulti("urls", existURLs)
+	if err != nil {
+		log.Info().Err(err)
 	}
 	return sb.String()
 }
@@ -50,7 +73,7 @@ func main() {
 		fmt.Fprint(w, filter(file))
 	})
 	log.Info().Msg("Server started")
-	if err := http.ListenAndServe(":80", nil); err != nil {
+	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Info().Err(err).Msg("Startup failed")
 	}
 }
