bb5aaffcbc7f6d42df308f626006fd2ddfb7e6eb
Akmal <magchuz@pm.me>
2021-08-12 18:46:27 +0700

    Add better endpoints
    /deduplicate/ to dedupe
    /add/ to add url to filter
    /info/ to check bloom info

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/RedisBloom/redisbloom-go v1.0.0
 	github.com/gomodule/redigo v1.8.5
+	github.com/json-iterator/go v1.1.11
 	github.com/rs/zerolog v1.23.0
 	gopkg.in/yaml.v3 v3.0.0-20200605160147-a5ece683394c // indirect
 )
--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"net/http"
 	_ "net/http/pprof"
 	"strings"
 
 	redisbloom "github.com/RedisBloom/redisbloom-go"
 	"github.com/gomodule/redigo/redis"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -18,12 +18,22 @@ var (
 	pool = &redis.Pool{Dial: func() (redis.Conn, error) {
 		return redis.Dial("tcp", "localhost:6379", redis.DialPassword(""))
 	}}
-	client = redisbloom.NewClientFromPool(pool, "bloom-cah")
+	client = redisbloom.NewClientFromPool(pool, "")
+	json   = jsoniter.ConfigFastest
 )
 
-func filter(file io.Reader) string {
+func deduplicateHandler(w http.ResponseWriter, r *http.Request) {
 	var sb strings.Builder
 	var lines []string
+
+	r.ParseMultipartForm(1 * 1024 * 1024)
+	file, _, err := r.FormFile("file")
+	defer file.Close()
+	if err != nil {
+		log.Info().Err(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -31,17 +41,68 @@ func filter(file io.Reader) string {
 		lines = append(lines, scanner.Text())
 	}
 
-	result, err := client.BfAddMulti("urls", lines)
+	result, err := client.BfExistsMulti("urls", lines)
 	if err != nil {
 		log.Info().Err(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	for index, existNum := range result {
-		if existNum == 1 {
+		if existNum == 0 {
 			sb.WriteString(lines[index] + "\n")
 		}
 	}
-	return sb.String()
+	fmt.Fprint(w, sb.String())
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	var lines []string
+	r.ParseMultipartForm(1 * 1024 * 1024)
+	file, _, err := r.FormFile("file")
+	defer file.Close()
+	if err != nil {
+		log.Info().Err(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// Possible optimization
+		lines = append(lines, scanner.Text())
+	}
+
+	_, err = client.BfAddMulti("urls", lines)
+	if err != nil {
+		log.Info().Err(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	result, err := client.Info("urls")
+	if err != nil {
+		log.Info().Err(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(result["Capacity"])
+	resultMarshal, err := json.Marshal(result)
+	if err != nil {
+		log.Info().Err(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(resultMarshal)
+}
+
+func makeHandler(fn func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fn(w, r)
+	}
 }
 
 func main() {
@@ -50,17 +111,10 @@ func main() {
 		http.ListenAndServe(":6060", nil)
 	}()
 
-	http.HandleFunc("/bloom/", func(w http.ResponseWriter, r *http.Request) {
-		r.ParseMultipartForm(1 * 1024 * 1024)
-		file, _, err := r.FormFile("file")
-		defer file.Close()
-		if err != nil {
-			log.Info().Err(err)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		fmt.Fprint(w, filter(file))
-	})
+	http.HandleFunc("/deduplicate/", deduplicateHandler)
+	http.HandleFunc("/add/", addHandler)
+	http.HandleFunc("/info/", infoHandler)
+
 	log.Info().Msg("Server started")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Info().Err(err).Msg("Startup failed")
