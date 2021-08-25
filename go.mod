580b313c5967e810d24aae1eb2019f80652fcd05
Akmal <magchuz@pm.me>
2021-08-26 05:16:32 +0700

    Rewrite to use fasthttp

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/gomodule/redigo v1.8.5
 	github.com/json-iterator/go v1.1.11
 	github.com/rs/zerolog v1.23.0
+	github.com/valyala/fasthttp v1.29.0
 	gopkg.in/yaml.v3 v3.0.0-20200605160147-a5ece683394c // indirect
 )
--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"bufio"
-	"fmt"
-	"net/http"
-	_ "net/http/pprof"
 	"strings"
+	"unsafe"
 
 	redisbloom "github.com/RedisBloom/redisbloom-go"
 	"github.com/gomodule/redigo/redis"
 	jsoniter "github.com/json-iterator/go"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/reuseport"
 )
 
 var (
@@ -22,121 +22,129 @@ var (
 	json   = jsoniter.ConfigFastest
 )
 
-func deduplicateHandler(w http.ResponseWriter, r *http.Request) {
-	var sb strings.Builder
-	var lines []string
-	var duplicateCount uint64
+func b2s(b []byte) string {
+	/* #nosec G103 */
+	return *(*string)(unsafe.Pointer(&b))
+}
 
-	key := r.Form.Get("key")
-	if key != "main" && key != "clipped" && key != "urls" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	file, _, err := r.FormFile("file")
+func ctx2strings(ctx *fasthttp.RequestCtx) ([]string, error) {
+	var lines []string
+
+	header, err := ctx.FormFile("file")
 	if err != nil {
-		log.Info().Err(err).Msg("Error opening file")
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		return nil, err
 	}
+
+	file, err := header.Open()
 	defer file.Close()
+	if err != nil {
+		return nil, err
+	}
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		// Possible optimization
 		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
+
+func deduplicateHandlerFunc(ctx *fasthttp.RequestCtx) {
+	var sb strings.Builder
+
+	key := b2s(ctx.FormValue("key"))
+	lines, err := ctx2strings(ctx)
+	if err != nil {
+		ctx.Error(err.Error(), fasthttp.StatusBadRequest)
+		log.Info().Err(err).Msg("")
+		return
 	}
 
 	result, err := client.BfExistsMulti(key, lines)
 	if err != nil {
-		log.Info().Err(err).Msg("Redis Error BF.MEXISTS")
-		w.WriteHeader(http.StatusInternalServerError)
+		ctx.Error(err.Error(), fasthttp.StatusBadRequest)
+		log.Info().Err(err).Msg("")
 		return
 	}
 
 	for index, existNum := range result {
 		if existNum == 0 {
 			sb.WriteString(lines[index] + "\n")
-		} else {
-			duplicateCount++
 		}
 	}
-	fmt.Fprint(w, sb.String())
+
+	ctx.SetContentType("text/plain; charset=utf-8")
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	ctx.SetBodyString(sb.String())
 }
 
-func addHandler(w http.ResponseWriter, r *http.Request) {
-	var lines []string
-
-	key := r.Form.Get("key")
-	if key != "main" && key != "clipped" && key != "urls" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	file, _, err := r.FormFile("file")
+func addHandlerFunc(ctx *fasthttp.RequestCtx) {
+	key := b2s(ctx.FormValue("key"))
+	lines, err := ctx2strings(ctx)
 	if err != nil {
-		log.Info().Err(err).Msg("Error opening file")
-		w.WriteHeader(http.StatusBadRequest)
+		ctx.Error(err.Error(), fasthttp.StatusBadRequest)
+		log.Info().Err(err).Msg("")
 		return
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		// Possible optimization
-		lines = append(lines, scanner.Text())
 	}
 
 	_, err = client.BfAddMulti(key, lines)
 	if err != nil {
-		log.Info().Err(err).Msg("Redis Error BF.MADD")
-		w.WriteHeader(http.StatusInternalServerError)
+		ctx.Error(err.Error(), fasthttp.StatusBadRequest)
+		log.Info().Err(err).Msg("")
 		return
 	}
-	w.WriteHeader(http.StatusOK)
+
+	ctx.SetStatusCode(fasthttp.StatusOK)
 }
 
-func infoHandler(w http.ResponseWriter, r *http.Request) {
-	key := r.Form.Get("key")
-	if key != "main" && key != "clipped" && key != "urls" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+func infoHandlerFunc(ctx *fasthttp.RequestCtx) {
+	key := b2s(ctx.FormValue("key"))
 	result, err := client.Info(key)
 	if err != nil {
-		log.Info().Err(err).Msg("Redis Error BF.INFO")
-		w.WriteHeader(http.StatusInternalServerError)
+		ctx.Error(err.Error(), fasthttp.StatusBadRequest)
+		log.Info().Err(err).Msg("")
 		return
 	}
 
 	resultMarshal, err := json.Marshal(result)
 	if err != nil {
-		log.Info().Err(err).Msg("Error marshalling result")
-		w.WriteHeader(http.StatusInternalServerError)
+		ctx.Error(err.Error(), fasthttp.StatusBadRequest)
+		log.Info().Err(err).Msg("")
 		return
 	}
-	w.Write(resultMarshal)
-}
-
-func makeHandler(fn func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		r.ParseMultipartForm(1 * 1024 * 1024)
-		log.Info().Str("ip_address", r.RemoteAddr).Str("url", r.URL.Path).Msg("")
-		fn(w, r)
-	}
+	ctx.SetBody(resultMarshal)
+	ctx.SetStatusCode(fasthttp.StatusOK)
 }
 
 func main() {
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 
-	go func() {
-		http.ListenAndServe(":6060", nil)
-	}()
-
-	http.HandleFunc("/deduplicate/", makeHandler(deduplicateHandler))
-	http.HandleFunc("/add/", makeHandler(addHandler))
-	http.HandleFunc("/info/", makeHandler(infoHandler))
-
-	log.Info().Msg("Server started")
-	if err := http.ListenAndServe(":8000", nil); err != nil {
-		log.Info().Err(err).Msg("Startup failed")
+	m := func(ctx *fasthttp.RequestCtx) {
+		switch string(ctx.Path()) {
+		case "/deduplicate":
+			deduplicateHandlerFunc(ctx)
+		case "/add":
+			addHandlerFunc(ctx)
+		case "/info":
+			infoHandlerFunc(ctx)
+		default:
+			ctx.Error("not found", fasthttp.StatusNotFound)
+		}
 	}
+
+	ln, err := reuseport.Listen("tcp4", ":8000")
+	if err != nil {
+		log.Fatal().Msg(err.Error())
+	}
+
+	server := &fasthttp.Server{
+		Handler:                      m,
+		MaxRequestBodySize:           32 * 1024 * 1024,
+		ReduceMemoryUsage:            true,
+		DisablePreParseMultipartForm: true,
+	}
+	server.Serve(ln)
 }
